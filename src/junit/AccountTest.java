8d5259d65e0295dcdd12de83201370838483459f
SysAdmin <SysAdmin@THEDOCTOR.fios-router.home>
2017-11-05 08:07:22 -0500

    Added test to cover jenkins

--- src/junit/AccountTest.java
+++ src/junit/AccountTest.java
@@ -121,10 +121,19 @@ public class AccountTest {
 
 	@Test
 	public void getFormattedBalance() {
+		String expected = "$50";
 		Account account = new Account("O1001", "Checking", "50");
-		Assert.assertEquals("$50",account.getFormattedBalance() );		
+		Assert.assertEquals(expected,account.getFormattedBalance() );		
+		}
+
+/*	@Test
+	public void getFormattedBalance() {
+		String balance = "50.25";
+		Assert.assertEquals("$valid","$" + Account.validateBalance(balance));
+		String expected ="$50.25";
+		Assert.assertEquals(expected, "$" + balance);
 	}
-	
+	*/
 	@Test
 	public void accountDoesNotExistMessage() {
 		Assert.assertEquals("Account should not exist", "Invalid Account ID", Account.validateAccountExists("A1001"));
