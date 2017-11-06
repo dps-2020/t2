64f4910b838b88aa9d1f53f269072aa46e1fbf4b
LRE <LRE@LRELaptop.fios-router.home>
2017-11-05 22:39:17 -0500

    Testing Subtract

--- src/junit/AccountTest.java
+++ src/junit/AccountTest.java
@@ -110,6 +110,15 @@ public class AccountTest {
 	}
 	
 	@Test
+	public void subtract() {
+		Account account = new Account("O1001","Checking","50");
+		String tosubtract = "25.00" ;
+		account.subtract(tosubtract);
+		Assert.assertEquals("25.00", account.getBalance());
+	
+	}
+	
+	@Test
 	public void negativeBalance() {
 		String balance = "-50";
 		Assert.assertEquals("Balance cannot be negative",Account.validateBalance(balance) );		
